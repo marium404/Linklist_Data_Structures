6f3edaf23aa7831787e43ed3e44b437943f545ab
Maryam Asghar <56351104+marium404@users.noreply.github.com>
2020-11-29 00:22:29 +0500

    Update Double_Linklist.cpp

--- Double_Linklist.cpp
+++ Double_Linklist.cpp
@@ -8,7 +8,7 @@ struct node{
 	node*prev;
 	
 	node(int n){
-		data=n;
+		data = n;
 	}
 	
 };
@@ -18,78 +18,78 @@ node*tail;
 
 void create_list(int a1)
 {
-	node* nn=new node(a1);
+	node* nn = new node(a1);
 	
-	if(head==NULL){
-	head=tail=nn;
-	nn->prev=NULL;
-	nn->next=NULL;
+	if(head == NULL){
+	head = tail = nn;
+	nn->prev = NULL;
+	nn->next = NULL;
 	}	
 }
 
 void insertatstart(int a2){
-	node* nn=new node(a2);
-	head->prev=nn;
-	nn->next=head;
-	nn->prev=NULL;
-	head=nn;
+	node* nn = new node(a2);
+	head->prev = nn;
+	nn->next = head;
+	nn->prev = NULL;
+	head = nn;
 }
 
 void insertatend(int a3){
-	node* nn=new node(a3);
-	if(head==NULL){
-		cout<<"Create list first";		
+	node* nn = new node(a3);
+	if(head == NULL){
+		cout << "Create list first";		
 	}
 	else{
-		tail->next=nn;
-		nn->prev=tail->next;
-		tail=nn;
-		tail->next=NULL;
+		tail->next = nn;
+		nn->prev = tail->next;
+		tail = nn;
+		tail->next = NULL;
 	}
 }
 
 void insertatpos(int aa,int posi){
 
-	node* nn=new node(aa);
-	node* tmp=head;
+	node* nn = new node(aa);
+	node* tmp = head;
 	node* tmp2;
-	int i=1;
-	while(tmp!=NULL){
-		if(i==posi-1){
-			tmp2=tmp->next;
-			tmp->next=nn;
-			nn->prev=tmp->next;
+	int i = 1;
+	while(tmp != NULL){
+		if(i == posi - 1){
+			tmp2 = tmp->next;
+			tmp->next = nn;
+			nn->prev = tmp->next;
 			
-			tmp2->prev=nn->next;
-			nn->next=tmp2;	
+			tmp2->prev = nn->next;
+			nn->next = tmp2;	
 		}
 		
 		i++;
-		tmp=tmp->next;		
+		tmp = tmp->next;		
 	}	
 }
 
 
 void search(int a4)
 {
-	node* tmp=head;
-	while(tmp!=NULL)
+	node* tmp = head;
+	while(tmp != NULL)
 	{
-		if(tmp->data==a4)
+		if(tmp->data == a4)
 			{
-				cout<<"Value exist in list";
+				cout << "Value exist in list";
 		    	break;
 			}
 		    
-		    tmp=tmp->next;
+		    tmp = tmp->next;
 	}
 }
 
 
 void gtrcount(int a5,int count=0,int a=0)
 {
-	node* tmp=head;
-	while(tmp!=NULL)
+	node* tmp = head;
+	while(tmp != NULL)
 	{
 		if(tmp->data > a5)
 			{		
@@ -100,7 +100,7 @@ void gtrcount(int a5,int count=0,int a=0)
 		    
 			tmp = tmp->next;
 	}
-	cout<<"Greater numbers are:"<<a;	
+	cout << "Greater numbers are:" << a;	
 }
 
 
@@ -110,134 +110,138 @@ void deletefirst(){
         cout<<"List is already empty.";
     }
     else{
-    	node*tmp=head;
-    		head=tmp->next;
-    		head->prev=tmp->prev;
+    	node*tmp = head;
+    		head = tmp->next;
+    		head->prev = tmp->prev;
     		delete(tmp);
-    	cout<<"First node has being deleted.\n";
+    	cout << "First node has being deleted.\n";
 	}
 		
 	
 }
 
 void deletelast(){
-	node* tmp=head;
+	node* tmp = head;
 	
-	while(tmp!=NULL){
-		if(tmp->next==tail){
-			tmp->prev->next=NULL;
-			tail=tmp;
+	while(tmp != NULL){
+		if(tmp->next == tail){
+			tmp->prev->next = NULL;
+			tail = tmp;
 		}
 		
-		tmp=tmp->next;
+		tmp = tmp->next;
 	}
 	
-	cout<<"Last node has being deleted.\n";
+	cout << "Last node has being deleted.\n";
 }
 
 void delatposi(int posi){
-	node* tmp=head;
+	node* tmp = head;
 	node* tmp2;
 	node*tmp3;
-	int noofnode=1;
+	int noofnode = 1;
 	
-	while(tmp->next!=NULL){
+	while(tmp->next != NULL){
 
-		if(noofnode==posi-1){
-			tmp2=tmp->next;
-			tmp3=tmp2->next;
-			tmp->next=tmp3;
-			tmp3->prev=tmp;
-			tmp2->next=NULL;	
+		if(noofnode == posi - 1){
+			tmp2 = tmp->next;
+			tmp3 = tmp2->next;
+			tmp->next = tmp3;
+			tmp3->prev = tmp;
+			tmp2->next = NULL;	
 		}
 	noofnode++;
-	tmp=tmp->next;	
+	tmp = tmp->next;	
 	}
 }
 
 void display(){
-	node* tmp=head;
+	node* tmp = head;
 	
-	while(tmp!=NULL){
-		cout<<tmp->data<<"  ";
+	while(tmp != NULL){
+		cout << tmp->data << "  ";
 		
-		tmp=tmp->next;
+		tmp = tmp->next;
 	}
 }
 
 
 
 
-int main(){	
-	cout<<"\t\t\t****DOUBLE LINKLIST****"<<endl;
-	cout<<"\t1: Create list"<<endl;																		
-	cout<<"\t2: Insert new node at start"<<endl;			
-	cout<<"\t3: Insert node at the end"<<endl;	
-	cout<<"\t4: Insert the node in specific position"<<endl;				
-	cout<<"\t5: Search user given value in list"<<endl;	
-	cout<<"\t6: Count number of elements greater than user given number"<<endl;
-	cout<<"\t7: Delete first node"<<endl;								
-	cout<<"\t8: Delete element from last"<<endl;		
-	cout<<"\t9: Delete from specific position"<<endl;
-	cout<<"\t10: Display values of list"<<endl;			
-	cout<<"\t11: Exit\n"<<endl;	
+int main(){
+	
+	system("color 04");
+	
+	cout << "\t\t\t****DOUBLE LINKLIST****\n";
+	cout << "\t1: Create list\n";																		
+	cout << "\t2: Insert new node at start\n";			
+	cout << "\t3: Insert node at the end\n";	
+	cout << "\t4: Insert the node in specific position\n";				
+	cout << "\t5: Search user given value in list\n";	
+	cout << "\t6: Count number of elements greater than user given number\n";
+	cout << "\t7: Delete first node\n";								
+	cout << "\t8: Delete element from last\n";		
+	cout << "\t9: Delete from specific position\n";
+	cout << "\t10: Display values of list\n";			
+	cout << "\t11: Exit\n";
+	cout << "\t\t-------------------\n";
 	
 	int a,n,loop;
 	
 	while(true){
-		cout<<"\nEnter Choice"<<endl;
-		cin>>a;
+		cout << "\nEnter Choice\n";
+		cin >> a;
 	
 	
 	switch(a)
 	{
 		case 1:
 			
-			cout<<"Enter data to list:"<<endl;
-			cin>>n;
+			cout << "Enter data to list:\n";
+			cin >> n;
 			create_list(n);
 			
 			
-			cout<<"List has being created"<<endl;
+			cout << "List has being created\n";
 			display();
 			break;
 		
 		case 2:
-			cout<<"Enter value you want to insert at start:\n";
-			cin>>n;
+			cout << "Enter value you want to insert at start:\n";
+			cin >> n;
 			insertatstart(n);
-			cout<<"List is :"<<endl;
+			cout << "List is :\n";
 			display();
 			break;
 			
 		case 3:
-			cout<<"Enter value to insert at end:"<<endl;
-			cin>>n;
+			cout << "Enter value to insert at end:\n";
+			cin >> n;
 			insertatend(n);
-			cout<<"List is :"<<endl;
+			cout << "List is :\n";
 			display();
 			break;
 			
 		case 4:
 			int i;
-			cout<<"Enter the position to add node:";
-			cin>>n;
-			cout<<"\nEnter value to insert:"<<endl;
-			cin>>i;
+			cout << "Enter the position to add node:\n";
+			cin >> n;
+			cout << "\nEnter value to insert:\n";
+			cin >> i;
 			insertatpos(i,n);
-			cout<<"List is :"<<endl;
+			cout << "List is :" << endl;
 			display();
 			break;
 			
 		case 5:
-			cout<<"\nEnter value you want to search:"<<endl;
-			cin>>n;
+			cout << "\nEnter value you want to search:"<< endl;
+			cin >> n;
 		    search(n);
 			break;
 			
 		case 6:
-			cout<<"\nEnter value by which you want to check greater numbr:"<<endl;
-			cin>>n;
+			cout << "\nEnter value by which you want to check greater numbr:" << endl;
+			cin >> n;
 			gtrcount(n);
 			
 			break;
@@ -246,34 +250,34 @@ int main(){
     	
     	case 7:
 			deletefirst();
-			cout<<"list is:"<<endl;
+			cout << "list is:\n";
 			display();	
 			break;
 			
 		case 8:
 			deletelast();
-			cout<<"list is:"<<endl;
+			cout << "list is:\n";
 			display();
 			break;
 			
 		case 9:
-			cout<<"Enter the position to del"<<endl;
-			cin>>n;
+			cout << "Enter the position to del\n";
+			cin >> n;
 			delatposi(n);
-				cout<<"list is:"<<endl;
+			cout << "list is:\n";
 			display();
 			break;
 					
 		case 10:
-			cout<<"List is:\n";
+			cout << "List is:\n";
 			display();
 			
 			
-	    case 11:
-	    	exit(0);
+	    	case 11:
+	    		exit(0);
 	    	
-	    default:
-	    	cout<<"\t\tInvalid output!";
+	    	default:
+	    		cout<<"\t\tInvalid output!";
 			
 	}
 
